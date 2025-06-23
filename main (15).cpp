e37d70a55adf4e8970105d24a8c8e7cc7b2ea59c
sruti-git <srutiselvi0411@gmail.com>
2025-06-23 18:00:04 +0530

    Add files via upload

---
+++ main (15).cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main() {
+    float radius;
+    cout << "Enter radius: ";
+    cin >> radius;
+    cout << "Area = " << 3.14 * radius * radius;
+    return 0;
+}
