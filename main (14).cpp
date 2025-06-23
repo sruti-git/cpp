6a8ab17dbfe368819139de6acf410633e4da2018
sruti-git <srutiselvi0411@gmail.com>
2025-06-23 17:57:21 +0530

    Add files via upload

---
+++ main (14).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    string name;
+    cin>>name>>age;
+    cout<<"name:"<<name<<endl;
+    cout<<"age:"<<age<<endl;
+    return 0;
+}
