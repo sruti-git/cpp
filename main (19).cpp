48cdd9fef9d5e8e2d27e68c9af9d28eedd9e6287
sruti-git <srutiselvi0411@gmail.com>
2025-08-06 18:17:58 +0530

    Add files via upload

---
+++ main (19).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    long long factorial = 1; 
+    cout << "Enter a number: ";
+    cin >> n;
+    if (n < 0) {
+        cout << "Factorial not defined for negative numbers." << endl;
+    } else {
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;  
+        }
+    cout << "Factorial of " << n << " is: " << factorial << endl;
+    }
+return 0;
+}
